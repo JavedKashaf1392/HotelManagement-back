3f30b71b817cf94ebd16e44673915ec8a6e39fb4
javedk <sreekanth.kona@ingredion.com>
2023-07-24 00:40:10 +0530

    initial changes

--- src/main/java/com/hotelmanagement/controller/BookingController.java
+++ src/main/java/com/hotelmanagement/controller/BookingController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,9 +47,9 @@ public class BookingController {
 	@Autowired
 	private UserService userService;
 	
-	@PostMapping("/")
+	@PostMapping("/save")
 	@ApiOperation(value = "Api to book hotel")
-	public ResponseEntity<?> register(Booking booking) {
+	public ResponseEntity<?> register(@RequestBody Booking booking) {
 		LOG.info("Recieved request for booking hotel");
 
 		System.out.println("booking data for adding hotel"+booking);
